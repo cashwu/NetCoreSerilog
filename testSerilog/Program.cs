7dc8d12e02f08993b47ca91394ab00e44997fff8
cash <cash@cashwu.com>
2020-01-08 13:27:19 +0800

    :tada: modify serilog setting

--- testSerilog/Program.cs
+++ testSerilog/Program.cs
@@ -11,48 +11,17 @@ namespace testSerilog
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            // var configuration = new ConfigurationBuilder()
-            //                     .SetBasePath(Directory.GetCurrentDirectory())
-            //                     .AddJsonFile("appsettings.json", false, true)
-            //                     .Build();
-            //
-            // Log.Logger = new LoggerConfiguration()
-            //              .ReadFrom.Configuration(configuration)
-            //              .CreateLogger();
-            
-            Log.Logger = new LoggerConfiguration()
-                         .MinimumLevel.Debug()
-                         .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                         .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning) 
-                         .Enrich.FromLogContext()
-                         .WriteTo.Console()
-                         .WriteTo.Seq("http://localhost:5341")
-                         .CreateLogger(); 
-
-            try
-            {
-                Log.Information("Starting web host");
-                CreateHostBuilder(args).Build().Run();
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly");
-
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog()
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration.ReadFrom.Configuration(context.Configuration);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
